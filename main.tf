54f5db44dc3a0f1ee731592343a905d4729f4775
Dmitry Kireev <dmitry@hazelops.com>
2021-10-26 21:39:02 +0300

    Refactor with this, output public_ip

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # Security Groups
-resource "aws_security_group" "nat_instance" {
+resource "aws_security_group" "this" {
   count       = var.enabled ? 1 : 0
   name        = "${var.env}-nat-instance"
   description = "Security Group for NAT EC2 Instance"
@@ -29,17 +29,17 @@ resource "aws_security_group" "nat_instance" {
 }
 
 # EC2
-resource "aws_instance" "nat_instance" {
+resource "aws_instance" "this" {
   count                  = var.enabled ? 1 : 0
-  ami                    = join("", data.aws_ami.nat_instance.*.id)
+  ami                    = join("", data.aws_ami.this.*.id)
   instance_type          = var.instance_type
   subnet_id              = var.public_subnets[0]
   key_name               = var.ec2_key_pair_name
-  vpc_security_group_ids = [aws_security_group.nat_instance[0].id]
+  vpc_security_group_ids = [aws_security_group.this[0].id]
 
-  source_dest_check = false
+  source_dest_check           = false
   associate_public_ip_address = true
-  
+
   lifecycle {
     create_before_destroy = true
   }
@@ -52,7 +52,7 @@ resource "aws_instance" "nat_instance" {
 
 }
 
-resource "aws_eip" "nat_instance" {
+resource "aws_eip" "this" {
   count = var.enabled ? 1 : 0
   vpc   = true
 
@@ -64,20 +64,20 @@ resource "aws_eip" "nat_instance" {
     "Name" = "${var.env}-nat-instance"
   }
 
-  depends_on = [aws_instance.nat_instance]
+  depends_on = [aws_instance.this]
 }
 
 resource "aws_eip_association" "nat_instance" {
   count         = var.enabled ? 1 : 0
-  instance_id   = element(aws_instance.nat_instance.*.id, count.index)
-  allocation_id = aws_eip.nat_instance[count.index].id
+  instance_id   = element(aws_instance.this.*.id, count.index)
+  allocation_id = aws_eip.this[count.index].id
 
-  depends_on    = [aws_eip.nat_instance]
+  depends_on = [aws_eip.this]
 }
 
-resource "aws_route" "nat_instance" {
+resource "aws_route" "this" {
   count                  = var.enabled ? 1 : 0
   route_table_id         = var.private_route_table_id
-  instance_id            = element(aws_instance.nat_instance.*.id, count.index)
+  instance_id            = element(aws_instance.this.*.id, count.index)
   destination_cidr_block = "0.0.0.0/0"
 }
